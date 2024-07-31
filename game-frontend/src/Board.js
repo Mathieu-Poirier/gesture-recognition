56e3adf80f1ced03fddcf06fd4fb42fc402a4f3a
Mathieu-Poirier <mathieuworkemail@gmail.com>
2024-07-31 00:40:00 -0400

    working on board

---
+++ game-frontend/src/Board.js
@@ -0,0 +1,24 @@
+export default function Square() {
+    return( 
+
+        <>
+            <div className="board-row">
+                <button  className="cell">1</button>
+                <button  className="cell">2</button>
+                <button  className="cell">3</button>
+            </div>
+            <div className="board-row">
+                <button  className="cell">4</button>
+                <button  className="cell">5</button>
+                <button  className="cell">6</button>
+            </div>
+            <div className="board-row">
+                <button  className="cell">7</button>
+                <button  className="cell">8</button>
+                <button  className="cell">9</button>
+            </div>
+        </>
+
+
+    ); 
+}
